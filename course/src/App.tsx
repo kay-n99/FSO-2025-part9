2512456d64adc8e040e5187fe1aa9a9b68eea276
kayn01 <kayanadonis@gmail.com>
2025-08-10 09:33:15 +0700

    exercises ~9.15

---
+++ course/src/App.tsx
@@ -0,0 +1,36 @@
+import Header from "./components/Header";
+import Content from "./components/Content";
+import Total from "./components/Total";
+
+type CoursePart = {
+  name: string;
+  exerciseCount: number;
+};
+
+const App = () => {
+  const courseName = "Half Stack application development";
+  const courseParts: CoursePart[] = [
+    {
+      name: "Fundamentals",
+      exerciseCount: 10
+    },
+    {
+      name: "Using props to pass data",
+      exerciseCount: 7
+    },
+    {
+      name: "Deeper type usage",
+      exerciseCount: 14
+    }
+  ];
+
+  return (
+    <div>
+      <Header name={courseName} />
+      <Content parts={courseParts} />
+      <Total total={courseParts.reduce((sum, part) => sum + part.exerciseCount, 0)} />
+    </div>
+  );
+};
+
+export default App;
